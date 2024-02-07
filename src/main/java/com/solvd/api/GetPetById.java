6212b51ac674da7733434aae3251e020a25f8ddc
elgaricito <edgarjaguirre@hotmail.com>
2024-02-07 19:11:45 -0300

    [SLIP-26] Corrected Method

--- src/main/java/com/solvd/api/GetPetById.java
+++ src/main/java/com/solvd/api/GetPetById.java
@@ -8,8 +8,8 @@ import com.zebrunner.carina.api.apitools.builder.NotStringValuesProcessor;
 import com.zebrunner.carina.api.http.HttpMethodType;
 import com.zebrunner.carina.api.http.HttpResponseStatusType;
 
-@Endpoint(url = "${config.api_url}/pet/100", methodType = HttpMethodType.GET)
-@ResponseTemplatePath(path = "api/pet/get_pet_by_id_response.json")
+@Endpoint(url = "${config.api_url}/pet/${petId}", methodType = HttpMethodType.GET)
+@ResponseTemplatePath(path = "api/pet/_gets/get_pet_by_id_response.json")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class GetPetById extends AbstractApiMethodV2 {
     public GetPetById(Integer id) {
