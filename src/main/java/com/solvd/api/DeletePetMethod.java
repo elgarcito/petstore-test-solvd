e5bb78eac6fd44fcfe62f8c3b513ee07ab3cb230
Pablo Rizzieri <ppablorizzieri@gmail.com>
2024-02-08 15:17:51 -0300

    [SLIP-30] Add DeletePetMethod to delete a pet by ID
    This commit introduces the DeletePetMethod class in the com.solvd.api package, which represents an API method for deleting a pet by its ID. The method uses the DELETE HTTP method and is mapped to the endpoint "${config.api_url}/pet/${petId}". The response is validated against the template located at "api/pet/_gets/get_pet_by_id_response.json". A successful HTTP status of 200 (OK) is expected. The method is constructed to handle pet deletion by specifying the pet ID during initialization.

---
+++ src/main/java/com/solvd/api/DeletePetMethod.java
@@ -0,0 +1,20 @@
+package com.solvd.api;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.apitools.builder.NotStringValuesProcessor;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+
+
+@Endpoint(url = "${config.api_url}/pet/${petId}", methodType = HttpMethodType.DELETE)
+@ResponseTemplatePath(path = "api/pet/_gets/get_pet_by_id_response.json")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+public class DeletePetMethod extends AbstractApiMethodV2 {
+    public DeletePetMethod(Integer id){
+        replaceUrlPlaceholder("petId", String.valueOf(id));
+        ignorePropertiesProcessor(NotStringValuesProcessor.class);
+    }
+}
